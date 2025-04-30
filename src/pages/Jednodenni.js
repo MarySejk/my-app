768737310b3d994834fdc20a3be20e1e91612c27
MarySejk <mariekohlova@gmail.com>
2025-04-30 14:15:20 +0200

    drobne upravy

--- src/pages/Jednodenni.js
+++ src/pages/Jednodenni.js
@@ -1,17 +1,22 @@
 import React from "react";
 /* import JednodenniSeznam from "../components/jednodenniSeznam"; */
 import Formular from "../components/Formular";
+import Footer from "../components/Footer";
 
 function Jednodenni() {
 
     return (
-        <div className="container-uvod">
-            <div className="textovy-blok">
-                <h2 className="nadpis">
-                    Zabal se na jeden den
-                </h2>
-                <Formular />
+        <div className="stranka" >
+            <div className="container-uvod">
+                <div className="textovy-blok">
+                    <h2 className="nadpis">
+                        Zabal se na jeden den
+                    </h2>
+                    <Formular />
+                </div>
             </div>
+
+            <Footer />
         </div>
     )
 }
--- src/pages/Vicedenni.js
+++ src/pages/Vicedenni.js
@@ -1,17 +1,20 @@
 import React from "react";
 import FormularViceDnu from "../components/FormularViceDnu";
-
+import Footer from "../components/Footer";
 
 function Vicedenni() {
     return (
-        <div className="container-uvod">
-            <div className="textovy-blok">
-                <h2 className="nadpis">
-                    Zabal se na více dnů
-                </h2>
-               <FormularViceDnu />
-                
+        <div>
+            <div className="container-uvod">
+                <div className="textovy-blok">
+                    <h2 className="nadpis">
+                        Zabal se na více dnů
+                    </h2>
+                    <FormularViceDnu />
+                </div>
             </div>
+
+            <Footer />
         </div>
     )
 }
